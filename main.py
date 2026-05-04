7e1fc687b780682023cfa4e47baef64e04c4e124
RightFix <righteousnessude@gmail.com>
2026-05-04 12:03:55 +0100

    Convert from KivyMD to pure Kivy with rounded buttons

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from kivymd.uix.screen import MDScreen
-from kivymd.app import MDApp
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.graphics import Color, RoundedRectangle
 from random import randint
 
-class Screen1(MDScreen):
-  pass
-class Screen2(MDScreen):
-   pass
-        
-class MainApp(MDApp):
-    def build(self):
-      return super().build()
-      
+
+class RoundedButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.bind(pos=self._update_canvas, size=self._update_canvas)
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(0.6, 0.6, 0.6, 1)
+            self.rrect = RoundedRectangle(size=self.size, pos=self.pos, radius=[20])
+
+    def _update_canvas(self, *args):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(0.6, 0.6, 0.6, 1)
+            RoundedRectangle(size=self.size, pos=self.pos, radius=[20])
+
+
+class Screen1(Screen):
+    pass
+
+
+class Screen2(Screen):
+    pass
+
+
+class MainApp(App):
     ran = randint
-        
+
+
 MainApp().run()
